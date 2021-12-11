e5af9f4afaa742c803b1be5827d5826a572f7c3d
volhashax <volhashax@gmail.com>
2021-12-11 18:58:27 +0300

    initial

---
+++ CalculateSquareRoots/CalculateSquareRoots.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace it_academy
+{
+    public enum RootExistanceEnum
+    {
+        NotDefined,
+        NoRoot,
+        OneRoot,
+        TwoRoot
+    }
+
+    public class CalculateSquareRoots
+    {
+        public float x1, x2;
+
+        public RootExistanceEnum rootExistance = RootExistanceEnum.NotDefined;
+
+        public void Calculate(float a, float b, float c)
+        {
+            if (a == 0)
+            {
+                rootExistance = RootExistanceEnum.NoRoot;
+                return;
+            }
+
+            var d = b * b - 4 * a * c;
+
+            if (d < 0)
+            {
+                rootExistance = RootExistanceEnum.NoRoot;
+            }
+            else if (d == 0)
+            {
+                rootExistance = RootExistanceEnum.OneRoot;
+            }
+            else
+            {
+                rootExistance = RootExistanceEnum.TwoRoot;
+            }
+
+        }
+    }
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,76 @@
+using System;
+using it_academy;
+using Xunit;
+
+namespace TestProject
+{
+    public class UnitTest1
+    {
+        /*
+         ax2 + bx + c = 0
+
+        D = b^2 - 4ac
+
+        D < 0 : no roots : a != 0; b=0,a!=0,c!=0; b = 1,a = 0.25,c = 1.1
+
+        D == 0 : one root =>  -b/2a : b = 1,a=0.25,c=1  
+
+        D > 0 : (-b + Sqrt(D))/2a, (-b - Sqrt(D))/2a
+            
+                 
+         */
+
+        [Fact]
+        public void Calculate_AIsZero_NoRoot()
+        {
+            // arrange
+            var cut = new it_academy.CalculateSquareRoots();
+
+            // act
+            cut.Calculate(0, 0, 0);
+
+            // assert
+            Assert.Equal(RootExistanceEnum.NoRoot, cut.rootExistance);
+        }
+
+        [Fact]
+        public void Calculate_BisZeroAisNotZeroCisNotZero_NoRoot()
+        {
+            // arrange
+            var cut = new it_academy.CalculateSquareRoots();
+
+            // act
+            cut.Calculate(1, 0, 1);
+
+            // assert
+            Assert.Equal(RootExistanceEnum.NoRoot, cut.rootExistance);
+        }
+
+        [Fact]
+        public void Calculate_BSquareLess4ac_NoRoot()
+        {
+            // arrange
+            var cut = new it_academy.CalculateSquareRoots();
+
+            // act
+            cut.Calculate(0.25f, 1, 1.1f);
+
+            // assert
+            Assert.Equal(RootExistanceEnum.NoRoot, cut.rootExistance);
+        }
+
+        [Fact]
+        public void Calculate_BisZero_OneRoot()
+        {
+            // arrange
+            var cut = new it_academy.CalculateSquareRoots();
+
+            // act
+            cut.Calculate(0.25f, 1, 1f);
+
+            // assert
+            Assert.Equal(RootExistanceEnum.OneRoot, cut.rootExistance);
+        }
+
+    }
+}
